b49d6f86f707da9c508db06109966c68441637a7
David95406 <83376469+David95406@users.noreply.github.com>
2025-06-22 11:49:27 +0200

    init

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue"
+
+const count = ref(0)
+</script>
+
+<template>
+  <header>
+    <h1>Hello World!</h1>
+  </header>
+  <main>
+    <button @click="count++">Click me: {{ count }}</button>
+  </main>
+  <footer>
+    <p>Hereg</p>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
