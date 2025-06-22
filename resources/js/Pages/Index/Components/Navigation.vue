84d0f164d5aa2684df14e4e53eb03cdc2d3f3e3a
David95406 <83376469+David95406@users.noreply.github.com>
2025-06-22 22:38:06 +0200

    heregFrontend

---
+++ resources/js/Pages/Index/Components/Navigation.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { router } from '@inertiajs/vue3'
+import { route } from 'ziggy-js'
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
+import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+
+const navigation = [
+    { name: 'Kezdőlap', href: '/', current: true },
+    { name: 'Rólunk', href: '#', current: false },
+    { name: 'Foglalás', href: '#', current: false },
+]
+
+
+const redirectTo = (url) => {
+    alert("ok")
+    router.get(route(url))
+}
+</script>
+<template>
+    <Disclosure as="nav" class="bg-linear-to-b from-white to-light-blue" v-slot="{ open }">
+        <div class="px-2 sm:px-6 lg:px-8">
+            <div class="max-w-full relative flex h-16 items-center">
+                <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                    <!-- Mobile menu button-->
+                    <DisclosureButton
+                        class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
+                        <span class="absolute -inset-0.5" />
+                        <span class="sr-only">Open main menu</span>
+                        <Bars3Icon v-if="!open" class="block size-6" aria-hidden="true" />
+                        <XMarkIcon v-else class="block size-6" aria-hidden="true" />
+                    </DisclosureButton>
+                </div>
+                <div class="flex shrink-0 items-center">
+                    <p>Logo helye</p>
+                </div>
+                <div class="hidden sm:flex flex-1 items-center justify-center">
+                    <div class="flex space-x-90">
+                        <a v-for="item in navigation" :key="item.name" :href="item.href"
+                            class="font-bold text-3xl pb-1 relative group"
+                            :class="[item.current ? 'text-black' : 'text-gray-400 hover:text-black']"
+                            style="letter-spacing: 0.4rem;" :aria-current="item.current ? 'page' : undefined">
+                            {{ item.name }}
+                            <div class="absolute bottom-0 right-1 w-1/2 h-1 transition-colors duration-300"
+                                :class="[item.current ? 'bg-orange-500' : 'bg-transparent group-hover:bg-orange-500']">
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <DisclosurePanel class="sm:hidden">
+            <div class="space-y-1 px-2 pt-2 pb-3">
+                <DisclosureButton v-for="item in navigation" :key="item.name" as="button" @click="alert(item.href)"
+                    :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"
+                    :aria-current="item.current ? 'page' : undefined">
+                    {{ item.name }}
+                </DisclosureButton>
+            </div>
+        </DisclosurePanel>
+    </Disclosure>
+</template>
+<style scoped></style>
--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,27 +1,11 @@
 <script setup>
-import { ref } from "vue"
-import { router } from "@inertiajs/vue3";
-import { route } from "ziggy-js";
-const count = ref(0)
+import Navigation from './Components/Navigation.vue';
 
-const redirectToAdmin = () => {
-  router.get(route('admin.index'))
-}
 </script>
 
 <template>
-  <header>
-    <h1>Hello World!</h1>
-  </header>
-  <main>
-    <button @click="count++">Click me: {{ count }}</button>
-    <button @click="redirectToAdmin()">Admin site</button>
-  </main>
-  <footer>
-    <p>Hereg</p>
-  </footer>
+  <Navigation />
 </template>
 
 <style scoped>
-
 </style>
